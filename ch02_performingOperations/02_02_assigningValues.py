b9ac9f300eca7f525a420173f2ed67e9439bddb9
jmireles2dev <j.mireles2nd.dev@gmail.com>
2022-04-15 17:34:59 -0500

    Exercise in Progress

---
+++ ch02_performingOperations/02_02_assigningValues.py
@@ -0,0 +1,18 @@
+# Assigning values
+# 02_01_doingArithmetic.py
+
+# Operator      Example     Equivalent
+#  =            a = b       a = b
+#  +=           a += b      a = (a + b)
+#  -=           a -= b      a = (a - b)
+#  *=           a *= b      a = (a * b)
+#  /=           a /= b      a = (a / b)
+#  %=           a %= b      a = (a % b)
+#  //=          a //= b     a = (a // b)
+#  **=          a **= b     a = (a ** b)
+
+# Define variables a and b
+a = 8
+b = 4
+
+print('Assigned Values:\t\t', 'a =', a, '\tb =', b)
