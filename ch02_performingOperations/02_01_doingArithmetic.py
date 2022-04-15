7a5820d4806b9e6f272b77d11f227b1c513106a2
jmireles2dev <j.mireles2nd.dev@gmail.com>
2022-04-15 14:42:06 -0500

    Exercise Complete

---
+++ ch02_performingOperations/02_01_doingArithmetic.py
@@ -0,0 +1,30 @@
+# Doing Arithmetic
+# 02_01_doingArithmetic.py
+
+# Common operators
+# +  Addition
+# -  Subtraction
+# *  Multiplication
+# /  Division
+# %  Modulo
+# // Floor division
+# ** Exponent
+
+# Initialize Integer Values
+a = 8
+b = 2
+
+# Operation Examples
+print('Addition:\t', a, '+', b, '=', a + b)
+
+print('Subtraction:\t', a, '-', b, '=', a - b)
+
+print('Multiplication:\t', a, '*', b, '=', a * b)
+
+print('Divison:\t', a, '÷', b, '=', a / b)
+
+print('Floor Division:\t', a, '÷', b, '=', a // b)
+
+print('Modulo:\t\t', a, '%', b, '=', a % b)
+
+print('Exponent:\t ', a, '^2 = ', a ** b, sep='')
